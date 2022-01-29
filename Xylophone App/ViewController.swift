a43b1333e66e4eb4ee38323cdd26286a509b2776
Gospodi <gospodijazzet@gmail.com>
2022-01-29 17:39:29 +0300

    подключил проигрывание звука на первую клавишу (C key)

--- Xylophone App/ViewController.swift
+++ Xylophone App/ViewController.swift
@@ -9,14 +9,34 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view. (Выполните любую дополнительную настройку после загрузки представления.)
     }
     
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("Я тебя нажал!")
+        
+        playSound(soundName: sender.currentTitle!)
+        
+        //Reduces the sender's (the button that got pressed) opacity to half. (Уменьшает непрозрачность отправителя (кнопки, которая была нажата) до половины.)
+        sender.alpha = 0.5
+        
+        //Code should execute after 0.2 second delay. (Код должен выполняться с задержкой в 0,2 секунды.)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.(Верните непрозрачность отправителя к полной непрозрачности.)
+            sender.alpha = 1.0
+        }
+        
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+        
     }
     
 
